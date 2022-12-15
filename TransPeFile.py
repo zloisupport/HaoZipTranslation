73aeb59a76022009de7daea3ea9f8ffac1e26c84
zloisupport <zlo1supp0rt@gmail.com>
2022-12-15 23:20:28 +0600

    minor changes

--- TransPeFile.py
+++ TransPeFile.py
@@ -2,24 +2,26 @@ from genericpath import isfile
 import re
 import subprocess
 import os
-
+import json
 """
 
 	HaoZipLang changing function
 
 """
-def langDll():
-	dirPathDialog= "D:\\repos\\HaoZipTranslation\\lng\\en\\lang\\HaoZipLang_chs\\Dialog"
-	dirPathPng= "D:\\repos\\HaoZipTranslation\\lng\\en\\lang\\HaoZipLang_chs\\PNG"
-	dirPathString= "D:\\repos\\HaoZipTranslation\\lng\\en\\lang\HaoZipLang_chs\\String"
-	dirPathMenu= "D:\\repos\\HaoZipTranslation\\lng\\en\\lang\\HaoZipLang_chs\\Menu"
+def langDll(path,lang,workdir):
+	dirPathDialog= f"{path}\\lng\\{lang}\\lang\\HaoZipLang_chs\\Dialog"
 
-	dirPathBMP= "D:\\repos\\HaoZipTranslation\\lng\\en\\HaoZip\\Bitmap"
 
-	dirHaoCDDialog ="D:\\repos\\HaoZipTranslation\\lng\\en\\HaoZipCD\\Dialog"
-	dirHaoCDString ="D:\\repos\\HaoZipTranslation\\lng\\en\\HaoZipCD\\String"
+	print(dirPathDialog)
+	dirPathPng= f"{path}\\lng\\{lang}\\lang\\HaoZipLang_chs\\PNG"
+	dirPathString= f"{path}\\lng\\{lang}\\lang\HaoZipLang_chs\\String"
+	dirPathMenu= f"{path}\\lng\\{lang}\\lang\\HaoZipLang_chs\\Menu"
 
-	workPath = "D:\\repos\\temp"
+	dirPathBMP= f"{path}\\lng\\{lang}\\HaoZip\Bitmap"
+
+	dirHaoCDDialog =f"{path}\\lng\\{lang}\\HaoZipCD\\Dialog"
+	dirHaoCDString =f"{path}\\lng\\{lang}\\HaoZipCD\\String"
+
 	
 	resDialog = getFiles(dirPathDialog)
 	resPng = getFiles(dirPathPng)
@@ -31,50 +33,66 @@ def langDll():
 
 
 	# HaoZipLang_chs.dll
-	setAssignLang(workPath,"Dialog",dirPathDialog,resDialog)
-	setAssignLang(workPath,"PNG",dirPathPng,resPng)
-	setAssignLang(workPath,"String",dirPathString,resString)
-	setAssignLang(workPath,"Menu",dirPathMenu,resMenu)
+	setAssignLang(workdir,"Dialog",dirPathDialog,resDialog)
+	setAssignLang(workdir,"PNG",dirPathPng,resPng)
+	setAssignLang(workdir,"String",dirPathString,resString)
+	setAssignLang(workdir,"Menu",dirPathMenu,resMenu)
 	# HaoZip.exe
-	setAssignHaoCD(workPath,"String",dirHaoCDString,resHaoCDString)
-	setAssignHaoCD(workPath,"Dialog",dirHaoCDDialog,resHaoCDDialog)
+	setAssignHaoCD(workdir,"String",dirHaoCDString,resHaoCDString)
+	setAssignHaoCD(workdir,"Dialog",dirHaoCDDialog,resHaoCDDialog)
 	# HaoZipCD.exe
-	setAssignHao(workPath,dirPathBMP,resHaoBmp)
+	setAssignHao(workdir,dirPathBMP,resHaoBmp)
 
 
 
-def setAssignLang(workPath,type,dirPath,res):
+def setAssignLang(workdir,type,dirPath,res):
 	for x in res:
 		print(x)
 		if type == "String":
-			subprocess.run(f'Restorator.exe -open "{workPath}\\lang\\HaoZipLang_chs.dll" -nobackup -delete String -assign "{dirPath}\\{x}" -save -exit')
+			subprocess.run(f'Restorator.exe -open "{workdir}\\lang\\HaoZipLang_chs.dll" -nobackup -delete String -assign "{dirPath}\\{x}" -save -exit')
 		else:
-			subprocess.run(f'Restorator.exe -open "{workPath}\\lang\\HaoZipLang_chs.dll" -nobackup -delete {type}\{x[:-3]} -assign "{dirPath}\\{x}" -save -exit')
+			subprocess.run(f'Restorator.exe -open "{workdir}\\lang\\HaoZipLang_chs.dll" -nobackup -delete {type}\{x[:-3]} -assign "{dirPath}\\{x}" -save -exit')
 
 
-def setAssignHaoCD(workPath,type,dirPath,res):
+def setAssignHaoCD(workdir,type,dirPath,res):
 	for x in res:
 		print(x)
 		if type == "String":
-			subprocess.run(f'Restorator.exe -open "{workPath}\\HaoZipCD.exe" -nobackup -delete String -assign "{dirPath}\\{x}" -save -exit')
+			subprocess.run(f'Restorator.exe -open "{workdir}\\HaoZipCD.exe" -nobackup -delete String -assign "{dirPath}\\{x}" -save -exit')
 		else:
-			subprocess.run(f'Restorator.exe -open "{workPath}\\HaoZipCD.exe" -nobackup -delete {type}\{x[:-3]} -assign "{dirPath}\\{x}" -save -exit')
+			subprocess.run(f'Restorator.exe -open "{workdir}\\HaoZipCD.exe" -nobackup -delete {type}\{x[:-3]} -assign "{dirPath}\\{x}" -save -exit')
 
 
-def setAssignHao(workPath,dirPath,res):
+def setAssignHao(workdir,dirPath,res):
 	for x in res:
 		print(x)
-		subprocess.run(f'Restorator.exe -open "{workPath}\\HaoZip.exe" -nobackup -assign "{dirPath}\\{x}" -save -exit')
+		subprocess.run(f'Restorator.exe -open "{workdir}\\HaoZip.exe" -nobackup -assign "{dirPath}\\{x}" -save -exit')
 
 
 
 def getFiles(dirPath):
 	res = []
-	for path in os.listdir(dirPath):
-		if os.path.isfile(os.path.join(dirPath,path)):
-			res.append(path)
+	for file in os.listdir(dirPath):
+		if os.path.isfile(os.path.join(dirPath,file)):
+			res.append(file)
 	return res
 
+if __name__ == "__main__":
+	confFile = "conf.json"
+	if not os.path.exists(confFile):
+		print('conf.json: not found')
+		defaultData ={"path":"D:\\repos\\HaoZipTranslation", "lang": "en", "workdir":"D:\\repos\\temp"}
+		
+		print('creatig: conf.json ')
+		with open("conf.json","w",) as outFile:
+			json.dump(defaultData,outFile,indent=4)
+			outFile.close
+	else:
+		print('read: conf.json ')
+		data = json.load(open(confFile,'r'))
 
-
-langDll()
+		path = data['path']
+		lang = data['lang']
+		workdir = data['workdir']
+		print(f'conf: {path,lang,workdir}')
+		langDll(path,lang,workdir)
